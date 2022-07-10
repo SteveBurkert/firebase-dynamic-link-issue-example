4c8f2859cf0a55b5ac00277b1f8bef48769b44a2
SteveBurkert <steve.burkert.nwo@gmail.com>
2022-07-10 15:21:41 +0200

    Add sign in code

--- app/src/main/java/com/example/firebase/zombie/MainActivity.java
+++ app/src/main/java/com/example/firebase/zombie/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.firebase.zombie;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.firebase.zombie.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+@SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity {
 
     private static final String testMail = "me@example.com";
@@ -29,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         // 3. if not, sign in anonymously
 
         initializeFirebaseAuth();
+        binding.buttonSignIn.setOnClickListener(view -> signInTestUser());
+        binding.buttonSignInAnon.setOnClickListener(view -> signInAnonymously());
+        binding.buttonSignOut.setOnClickListener(view -> signOut());
     }
 
     private void initializeFirebaseAuth() {
@@ -36,41 +42,84 @@ public class MainActivity extends AppCompatActivity {
         // 1. unknown -> FirebaseUser == null
         // 2. no user -> FirebaseUser == null
         // 3. signed in -> -> FirebaseUser != null
+        log("Initialize FirebaseAuth.state");
         FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 firebaseAuth.removeAuthStateListener(this);
-                log("FirebaseAuth initialized. Will now check authorization.");
+                binding.tvAuthInit.setText("true");
+                log("FirebaseAuth.state initialized");
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     log("Found a FirebaseUser");
-                    // verifyRegistration(user);
+                    displayUserState(user);
                 } else {
                     log("No firebase authenticated user found.");
-                    signInAnonymously(firebaseAuth);
+                    signInAnonymously();
                 }
             }
         });
     }
 
-    private void signInAnonymously(@NonNull final FirebaseAuth firebaseAuth) {
-
+    private void signInAnonymously() {
+        log("Going to sign in anonymously.");
+        final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInAnonymously().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                log("Successful signed in anonymously.");
+                FirebaseUser anonUser = firebaseAuth.getCurrentUser();
+                if (anonUser != null) {
+                    displayUserState(anonUser);
+                } else {
+                    log("Error: FirebaseUser == null after anon authentication.");
+                }
+            } else {
+                log("Error: Could not sign in anonymously." + task.getException());
+            }
+        });
     }
 
     private void signInTestUser() {
-
+        log("Going to sign in test user.");
+        final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(testMail, testPW).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                log("Successful signed in with email.");
+                FirebaseUser anonUser = firebaseAuth.getCurrentUser();
+                if (anonUser != null) {
+                    displayUserState(anonUser);
+                } else {
+                    log("Error: FirebaseUser == null after email authentication.");
+                }
+            } else {
+                log("Error: Could not sign in with email." + task.getException());
+            }
+        });
     }
 
     private void signOut() {
-
+        log("Going to sign out.");
+        FirebaseAuth.getInstance().signOut();
     }
 
     private void displayUserState(@NonNull FirebaseUser user) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("FirebaseUser:\n");
+        builder.append("isAnonymous: ").append(user.isAnonymous()).append("\n");
+        builder.append("Auth providers {").append("\n");
+        for (UserInfo info : user.getProviderData()) {
+            builder.append("ProviderId: ").append(info.getProviderId());
+        }
+        builder.append("}");
+
+        binding.tvUserAvailable.setText(builder.toString());
 
     }
 
     private void log(@NonNull final String message) {
         Log.d("MainActivity", message);
+        String oldLod = binding.tvLog.getText().toString();
+        binding.tvLog.setText(oldLod + "\n" + message);
     }
 
 }
