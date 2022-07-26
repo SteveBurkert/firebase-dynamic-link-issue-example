36c1509412c7e5aadb48301f02f8a499b9ab0d9d
SteveBurkert <steve.burkert.nwo@gmail.com>
2022-07-26 17:25:10 +0200

    Prepare example application for dynamic_link_app_open

--- README.md
+++ README.md
@@ -4,12 +4,36 @@ This repo only intents to showcase a problem related to the firebase android sdk
 
 Dependencies:
 
-firebase authentication:
-firebase bom:
+platform     : 'com.google.firebase:firebase-bom:29.1.0', // Updated to 30.1.0
+analytics    : 'com.google.firebase:firebase-analytics', // before update resolves to 20.1.0 - after to 21.0.0
+
+We updated the bom from 29.1.0 to 30.1.0 and released a new version.
+Since then we did not receive `dynamic_link_first_open` events anymore.
+
+After downgrading the bom back to 29.1.0 for the next release,
+the event is being tracked again.
 
 ## HowTo
 
+In order to reproduce, make sure: 
+- do not open the application on install
+- install first with an old version of bom (see above) and enable debug log
+- see that `dynamic_link_first_open` is tracked with old version 
 
+
+Console
+
+`adb shell setprop log.tag.FA VERBOSE`
+
+`adb shell setprop log.tag.FA-SVC VERBOSE`
+
+Open logcat:
+
+`adb logcat -v time -s FA FA-SVC`
+
+Next Step is ti open the application using the dynamic link `https://repoexmaple.page.link/test` 
+
+`dynamic_link_first_open` will get logged or not (Logged when a user opens the app for the first time via a Dynamic Link.) 
 
 
 
--- app/src/main/java/com/example/firebase/zombie/MainActivity.java
+++ app/src/main/java/com/example/firebase/zombie/MainActivity.java
@@ -1,27 +1,20 @@
 package com.example.firebase.zombie;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 
 import com.example.firebase.zombie.databinding.ActivityMainBinding;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.EmailAuthProvider;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity {
 
-    private static final String testMail = "me@example.com";
-    private static final String testPW   = "123456789";
+    // Test dynamic link: https://repoexmaple.page.link/test
 
     private ActivityMainBinding binding;
 
@@ -31,164 +24,32 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        // 1. init auth
-        // 2. sign in anonymously
-        // 3. Wait for user to sign in with real account and link it to anon
-
-        initializeFirebaseAuth();
-        setupButtons();
-        binding.tvLog.setMovementMethod(new ScrollingMovementMethod());
+        checkDynamicLink(getIntent());
     }
 
-    private void setupButtons() {
-        binding.buttonSignIn.setOnClickListener(view -> signInTestUser());
-        binding.buttonSignInAnon.setOnClickListener(view -> signInAnonymously());
-        binding.buttonSignOut.setOnClickListener(view -> signOut());
-        binding.buttonReset.setOnClickListener(view -> resetExample());
-        binding.buttonClearLog.setOnClickListener(view -> clearLog());
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        checkDynamicLink(intent);
     }
 
-    private void initializeFirebaseAuth() {
-        FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                firebaseAuth.removeAuthStateListener(this);
-                binding.tvAuthInit.setText("FirebaseAuth initialized: true");
-                log("FirebaseAuth.state initialized");
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    log("Found a FirebaseUser.");
-                    displayUserState(user);
+    private void checkDynamicLink(final Intent intent) {
+        if (intent != null) {
+            // Open and consume
+            FirebaseDynamicLinks.getInstance().getDynamicLink(intent).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    final PendingDynamicLinkData data = task.getResult();
+                    if (data != null && data.getLink() != null) {
+                        binding.textView.setText("App opened by dynamic: " + data.getLink());
+                    } else {
+                        binding.textView.setText("URI was null. No link in intent. Not opened by dynamic.");
+                    }
                 } else {
-                    log("No firebase authenticated user found.");
-                    signInAnonymously();
+                    binding.textView.setText("Could not load dynamic link: " + task.getException());
                 }
-            }
-        });
-    }
-
-    private void signInAnonymously() {
-        log("Going to sign in anonymously.");
-        final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        firebaseAuth.signInAnonymously().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                log("Successful signed in anonymously.");
-                FirebaseUser anonUser = firebaseAuth.getCurrentUser();
-                if (anonUser != null) {
-                    displayUserState(anonUser);
-                } else {
-                    log("Error: FirebaseUser == null after anon authentication.");
-                }
-            } else {
-                log("Error: Could not sign in anonymously." + task.getException());
-            }
-        });
-    }
-
-    private void signInTestUser() {
-        log("Going to sign in test user.");
-        final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        final FirebaseUser user = firebaseAuth.getCurrentUser();
-        if (user != null && !user.isAnonymous()) {
-            log("Error: Can't sign in, user is already authenticated");
-            return;
-        } else if (user == null) {
-            log("Error: For test purposes you can not sign in, without an anon account.");
-            return;
-        }
-
-        final AuthCredential credential = EmailAuthProvider.getCredential(testMail, testPW);
-
-        user.linkWithCredential(credential).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                log("Successful signed in with email and linked to anon account.");
-                FirebaseUser anonUser = firebaseAuth.getCurrentUser();
-                if (anonUser != null) {
-                    displayUserState(anonUser);
-                } else {
-                    log("Error: FirebaseUser == null after email authentication.");
-                }
-            } else {
-                final Exception e = task.getException();
-                log("Error: Could not link anon account with with email sign in." + e);
-                if (e instanceof FirebaseAuthUserCollisionException) {
-                    log("Use RESET (unlink) to restart the sample.");
-                }
-
-            }
-        });
-    }
-
-    /**
-     * Since I only want to use a dedicated test account, we have to delete the email
-     * account again or the next account linking will fail due to
-     * an FirebaseAuthUserCollisionException.
-     */
-    private void resetExample() {
-        log("Going to reset the example");
-        log("Going to delete email account.");
-        final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        firebaseAuth.signOut();
-        firebaseAuth.signInWithEmailAndPassword(testMail, testPW).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                final FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    user.delete().addOnCompleteListener(task1 -> {
-                        if (task1.isSuccessful()) {
-                            log("User is deleted. Restart.");
-                            clearLog();
-                            initializeFirebaseAuth();
-                        } else {
-                            log("Error. Reset failed. " + task1.getException());
-                        }
-                    });
-                } else {
-                    log("Error: Can't delete account. User == null.");
-                }
-            } else {
-                log("Error: Can't delete account. " + task.getException());
-            }
-        });
-    }
-
-    private void signOut() {
-        log("Going to sign out.");
-        FirebaseAuth.getInstance().signOut();
-        displayUserState(FirebaseAuth.getInstance().getCurrentUser());
-    }
-
-    private void displayUserState(@Nullable FirebaseUser user) {
-        if (user != null) {
-            final String userId = user.getUid();
-            StringBuilder builder = new StringBuilder();
-            builder.append("FirebaseUser:\n\n");
-            builder.append("id: ").append(userId.substring(0, 5)).append("...").append(userId.substring(userId.length() - 5)).append("\n");
-            builder.append("isAnonymous: ").append(user.isAnonymous()).append("\n");
-            builder.append("Auth providers {").append("\n");
-            for (UserInfo info : user.getProviderData()) {
-                builder.append("  ProviderId: ").append(info.getProviderId()).append("\n");
-            }
-            builder.append("}").append("\n");
-            boolean isInInvalidState = !user.isAnonymous() && user.getProviderData().size() == 1 && "firebase".equals(user.getProviderId());
-            builder.append("Has zombie state: ").append(isInInvalidState);
-            if (isInInvalidState) {
-                builder.append("\n").append("(The value of 'isAnonymous' should be true')");
-            }
-            binding.tvUserAvailable.setText(builder.toString());
+            });
         } else {
-            binding.tvUserAvailable.setText("FirebaseUser: null");
+            binding.textView.setText("No intent.");
         }
-
-    }
-
-    private void log(@NonNull final String message) {
-        Log.d("MainActivity", message);
-        String oldLod = binding.tvLog.getText().toString();
-        binding.tvLog.setText(oldLod + "\n" + message);
-    }
-
-    private void clearLog() {
-        binding.tvLog.setText("Log:");
     }
 }
